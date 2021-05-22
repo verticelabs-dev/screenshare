f514d2695aeda1bcee4d113253e694c3c559bd24
Travier Moorlag <moorlagt@gmail.com>
2021-05-22 11:32:00 -0400

    Pushing before hooking things back up

--- app/src/components/RoomControl.vue
+++ app/src/components/RoomControl.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col align-middle" style="max-width: 900px">
     <!-- Create Room -->
-    <div>
+    <div class="flex flex-row">
       <button
         class="btn btn-sm btn-primary pr-4"
         @click="initPeer"
@@ -9,7 +9,11 @@
       >
         Create Room
       </button>
-      <span> Code: {{ roomData.roomCode }} </span>
+      <div class="room-code-box">
+        <span class="text-center align-middle pt-1"
+          >Code: {{ roomData.roomCode }}</span
+        >
+      </div>
     </div>
     <!-- Join Room -->
     <div class="mt-4">
