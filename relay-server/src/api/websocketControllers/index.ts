6a36139c89e1f4af1090515a08633b5e73e400f6
Grey Delamar <grey.delamar@gmail.com>
2021-05-25 19:08:22 -0400

    🔥 remove not used file

--- relay-server/src/api/websocketControllers/index.ts
+++ relay-server/src/api/websocketControllers/index.ts
@@ -1,12 +1,10 @@
 import { Server } from "socket.io";
 import connection from './connection';
 import peer from './peer';
-import scale from './scale';
 
 export default (socketServer: Server) => {
   socketServer.on("connection", (socket) => {
     connection(socket);
     peer(socket);
-    scale(socket);
   });
 }
