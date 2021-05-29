92168d1f99b6aa3afce9b44eb6ea97dfa4ee9c42
Grey Delamar <grey.delamar@gmail.com>
2021-05-28 21:10:21 -0400

    ✨ Set stream on peer create if there is one

--- app/src/store/types.js
+++ app/src/store/types.js
@@ -3,6 +3,7 @@ const mutations = {
   SET_PEER_STREAM: "SET_PEER_STREAM",
   SET_ROOM_CODE: "SET_ROOM_CODE",
   SET_AUDIO_STREAM: "SET_AUDIO_STREAM",
+  SET_VIDEO_STREAM: "SET_VIDEO_STREAM",
 }
 
 export { mutations }
