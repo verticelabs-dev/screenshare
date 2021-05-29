f77da6e7661c00e62d2f2d046634741b3a13d2e6
Travier Moorlag <moorlagt@gmail.com>
2021-05-28 22:23:54 -0400

    Use cool new URL

--- app/src/services/SocketService.js
+++ app/src/services/SocketService.js
@@ -2,6 +2,6 @@ import io from "socket.io-client";
 
 // const socketServerURL = process.env.SOCKET_SERVER ?? "localhost:8989";
 
-const socket = io("tecdrip.com:8989");
+const socket = io("https://share-api.tecdrip.com");
 
 export default socket;
