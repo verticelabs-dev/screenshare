304066e3d03c520e916eb3317edbd428194d9f53
Grey Delamar <grey.delamar@gmail.com>
2021-05-31 20:01:56 -0400

    🐛 give axios a env for the base url

--- app/src/plugins/axios.js
+++ app/src/plugins/axios.js
@@ -8,7 +8,7 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: "/api",
+  baseURL: process.env.VUE_APP_API_URL || "/api",
   // timeout: 60 * 1000, // Timeout
   withCredentials: true, // Check cross-site Access-Control
 };
