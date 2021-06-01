6c29f88be0475bbb7b8f098b2a37638f9a17ebc0
Travier Moorlag <moorlagt@gmail.com>
2021-06-01 19:09:24 -0400

    cleaned up bad code

--- app/src/App.vue
+++ app/src/App.vue
@@ -37,7 +37,7 @@ export default {
 .slide-fade-leave-active {
   transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
 }
-.slide-fade-enter, .slide-fade-leave-to
+.slide-fade-enter
 /* .slide-fade-leave-active below version 2.1.8 */ {
   transform: translateX(10px);
   opacity: 0;
--- app/src/components/grid/Card.vue
+++ app/src/components/grid/Card.vue
@@ -1,5 +1,11 @@
 <template>
-  <div :class="{ 'card': true, 'screen-grid-item-lg': peer._peerID === activeScreenId }">
+  <div
+    class="card border"
+    :class="{
+      'screen-grid-item-lg': peer._peerID === activeScreenId,
+      [borderByAudioLevel]: true
+    }"
+  >
     <div class="card-header">
       <div class="text-white mb-1 mt-1 ml-4">{{ peer._peerID }}</div>
 
@@ -20,49 +26,66 @@
         ></div>
 
         <!-- Mute Button -->
-        <div
-          @click="toggleMute"
-          class="circle-btn"
-        >
-          <font-awesome-icon icon="volume-mute" v-if="muted"/>
-          <font-awesome-icon icon="volume-up" v-else/>
+        <div @click="toggleMute" class="circle-btn">
+          <font-awesome-icon icon="volume-mute" v-if="muted" />
+          <font-awesome-icon icon="volume-up" v-else />
         </div>
       </div>
     </div>
 
-    <div class="card-video">
-      <video :id="peer._peerID" :muted="muted ? 'muted': ''"></video>
+    <div class="card-video" v-if="false">
+      <video :id="peer._peerID" :muted="muted ? 'muted' : ''"></video>
+    </div>
+    <div class="flex items-center align-middle justify-center h-full" v-else>
+      <h1>You</h1>
     </div>
   </div>
 </template>
 
 <script>
 // import { mapState } from "vuex";
+// import { getAudioLevels } from "../../services/SoundMeter";
+// import { getAudioInput } from "../../services/StreamCaptureService";
 
 export default {
   props: ["id", "peer", "hideClose", "hideExpand", "activeScreenId"],
   components: {},
   watch: {},
-  computed: {},
-  mounted() {
+  computed: {
+    borderByAudioLevel() {
+      if (this.audioLevel == 0.0) {
+        return "border-secondary";
+      }
+
+      if (this.audioLevel >= 0.02) {
+        return "border-green-500";
+      }
+
+      return "border-secondary";
+    }
+  },
+  async mounted() {
     const self = this;
 
-    if (!self.peer || self.peer._peerID === "You") return
+    if (!self.peer || self.peer._peerID === "You") {
+      return;
+    }
 
-    self.peer.on("stream", (stream) => {
+    self.peer.on("stream", stream => {
       self.renderStream(stream);
     });
 
     // const stream = self.peer.streams[0];
-    // self.peer.on("track", (track) => {
-      // console.log('HIT', track)
-      // if (track.kind === "video" && stream) {
-      //   self.renderStream(stream);
-      // }
-    // });
+    // self.peer.on("track", track => {
+    // console.log('HIT', track)
+    // if (track.kind === "video" && stream) {
+    //   self.renderStream(stream);
+    // }
+    //});
   },
   data() {
     return {
+      audioLevel: 0,
       renderingStream: false,
       muted: false
     };
@@ -74,14 +97,17 @@ export default {
         video.srcObject = stream;
         video.play();
       } else {
-        console.log("Tried rendering stream", video , stream, this.peer);
+        console.log("Tried rendering stream", video, stream, this.peer);
       }
     },
     toggleMute() {
-      this.muted = !this.muted
+      this.muted = !this.muted;
     }
-  },
+  }
 };
 </script>
 
-<style></style>
+<style>
+.card {
+}
+</style>
