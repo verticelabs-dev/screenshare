4026cb1f085bb0c58470151e02b28621b37dc7a6
Grey Delamar <grey.delamar@gmail.com>
2021-06-02 10:34:13 -0400

    ✨ Show user info if already logged in

--- app/src/components/LoginDialog.vue
+++ app/src/components/LoginDialog.vue
@@ -1,45 +1,64 @@
 <template>
-  <Modal v-on:close="close">
-    <template v-slot:header> Login </template>
-    <template v-slot:body>
-      <div>
-        <label for="email" class="block font-medium">Email</label>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          autocomplete="email"
-          v-model="email"
-          class="mt-1 block w-full shadow-md rounded-md text-gray-700"
-        />
+  <div>
+    <Modal v-on:close="close" v-if="!user">
+      <template v-slot:header> Login </template>
+      <template v-slot:body>
+        <div>
+          <label for="email" class="block font-medium">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            autocomplete="email"
+            v-model="email"
+            class="mt-1 block w-full shadow-md rounded-md text-gray-700"
+          />
 
-        <br />
+          <br />
 
-        <label for="password" class="block font-medium">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          v-model="password"
-          @keyup.enter="login"
-          class="mt-1 block w-full shadow-md rounded-md text-gray-700"
-        />
-      </div>
-    </template>
-    <template v-slot:footer>
-      <button class="mt-4 btn btn-sm w-full btn-primary" @click="login">
-        Login
-      </button>
-    </template>
-  </Modal>
+          <label for="password" class="block font-medium">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            v-model="password"
+            @keyup.enter="login"
+            class="mt-1 block w-full shadow-md rounded-md text-gray-700"
+          />
+        </div>
+      </template>
+      <template v-slot:footer>
+        <button class="mt-4 btn btn-sm w-full btn-primary" @click="login">
+          Login
+        </button>
+      </template>
+    </Modal>
+    <Modal v-on:close="close" v-else>
+      <template v-slot:header> User Info </template>
+      <template v-slot:body>
+        <div>
+          <p>Full Name:</p>
+          <p class="mb-4">{{ user.full_name }}</p>
+          <p>Email:</p>
+          <p class="mb-4">{{ user.email }}</p>
+          <p>Joined At:</p>
+          <p class="mb-4">{{ user.created_at }}</p>
+        </div>
+      </template>
+    </Modal>
+  </div>
 </template>
 
 <script>
 import Modal from "./modal";
+import { mapState } from "vuex";
 
 export default {
   components: {
     Modal,
+  },
+  computed: {
+    ...mapState("user", ["user"]),
   },
   data() {
     return {
