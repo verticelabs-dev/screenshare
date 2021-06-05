5467afcf072cc6ede8462fd13b0c8e2f9472cb10
Grey Delamar <grey.delamar@gmail.com>
2021-06-05 16:08:54 -0400

    🐛 Better redis error if redis is down

--- relay-server/src/loaders/redis.ts
+++ relay-server/src/loaders/redis.ts
@@ -1,16 +1,16 @@
 import { createClient } from 'redis'
 import { promisify } from 'util';
 
-export const redis = createClient();
+const redis = createClient();
+
+redis.on("error", function () {
+  throw new Error('Redis is not answering')
+});
 
 export default () => {
   const setAsync = promisify(redis.set).bind(redis);
   const getAsync = promisify(redis.get).bind(redis);
   const delAsync = promisify(redis.del).bind(redis);
-
-  redis.on("error", function () {
-    throw new Error('Redis is not answering')
-  });
 
   return { ...redis, setAsync, getAsync, delAsync }
 };
