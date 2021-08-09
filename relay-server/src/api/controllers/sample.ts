4ff95ffac73c3243f24435a0abe0c8b31440ab28
Travier Moorlag <moorlagt@gmail.com>
2021-08-09 11:09:00 -0400

    Testing prod issues

--- relay-server/src/api/controllers/sample.ts
+++ relay-server/src/api/controllers/sample.ts
@@ -1,14 +1,15 @@
 // Dependencies
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response } from "express";
 const route = Router();
 
-
 export default (app: Router) => {
-  app.use('/', route);
+  app.use("/", route);
 
-  route.get('/ping', async (req: Request, res: Response) => {
+  route.get("/ping", async (req: Request, res: Response) => {
     // req.log.info('test');
 
-    return res.status(200).send({});
+    return res.status(200).send({
+      message: "pong",
+    });
   });
 };
